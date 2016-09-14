4d646b0e98600fc9c73bfce35b5a99f13a176fa2
selo796 <selcuk.imal@gmail.com>
2016-09-14 23:57:33 +0200

    enabled jQuery append analyzing

--- specs/attributeFinder/jquery/jQueryAttributeFinder.spec.js
+++ specs/attributeFinder/jquery/jQueryAttributeFinder.spec.js
@@ -4,7 +4,7 @@ let JQueryAttributeFinder = require('../../../src/attributeFinder/jQuery');
 
 describe('jQuery Attribute Finder Testing', function () {
     let s, util;
-    beforeEach(()=> {
+    beforeEach(() => {
         s = new JQueryAttributeFinder();
     });
     it('should reject an error if css file not found', (done) => {
@@ -32,7 +32,7 @@ describe('jQuery Attribute Finder Testing', function () {
             })
         );
         s.findAttribute('some.js').then((selectors) => {
-            expect(selectors).toEqual({ _class : [  ], _id : [  ] });
+            expect(selectors).toEqual({ _class: [], _id: [] });
             done();
         }, (err) => {
             expect(err).not.toBeDefined();
@@ -56,7 +56,7 @@ describe('jQuery Attribute Finder Testing', function () {
             })
         );
         s.findAttribute('some.js').then((selectors) => {
-            expect(selectors).toEqual({ _class : [  ], _id : [  ] });
+            expect(selectors).toEqual({ _class: [], _id: [] });
             done();
         }, (err) => {
             expect(err).not.toBeDefined();
@@ -72,7 +72,7 @@ describe('jQuery Attribute Finder Testing', function () {
             })
         );
         s.findAttribute('some.js').then((selectors) => {
-            expect(selectors).toEqual({ _class : [ 'selected' ], _id : [  ] });
+            expect(selectors).toEqual({ _class: ['selected'], _id: [] });
             done();
         }, (err) => {
             expect(err).not.toBeDefined();
@@ -88,7 +88,7 @@ describe('jQuery Attribute Finder Testing', function () {
             })
         );
         s.findAttribute('some.js').then((selectors) => {
-            expect(selectors).toEqual({ _class : [ 'selected', 'highlight' ], _id : [  ] });
+            expect(selectors).toEqual({ _class: ['selected', 'highlight'], _id: [] });
             done();
         }, (err) => {
             expect(err).not.toBeDefined();
@@ -105,7 +105,7 @@ describe('jQuery Attribute Finder Testing', function () {
             })
         );
         s.findAttribute('some.js').then((selectors) => {
-            expect(selectors).toEqual({ _class : [ 'selected', 'secondClass' ], _id : [  ] });
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass'], _id: [] });
             done();
         }, (err) => {
             expect(err).not.toBeDefined();
@@ -114,19 +114,152 @@ describe('jQuery Attribute Finder Testing', function () {
 
     });
 
-    xit('should resolve empty array found by looking "append(some html)"', (done) => {
+    it('should find all classes from "addClass" by using multiple addClasses', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p:last" ).addClass( "selected
+                secondClass" );
+                $( "p:last" ).addClass( "selected1
+                secondClass1" );`
+                );
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass', 'selected1', 'secondClass1'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
 
     });
 
-    xit('should return all attributes from addClass and append(someHtmlWithClass) "returns"', (done) => {
+    it('should find all classes from "addClass" by using multiple addClasses but only once', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p:last" ).addClass( "selected secondClass" );
+                $( "p:last" ).addClass( "selected
+                secondClass" );`
+                );
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
 
     });
 
-    xit('should find all ids from append(someHtmlWithId)', (done) => {
+    it('should resolve empty array found by looking "append(some html)" which does not have class attr', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("<strong>Hello</strong>" );`);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: [], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
+    });
 
+    it('should return one class attribute from append(someHtmlWithClass)', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("<strong class="selected">Hello</strong>" );`);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
+    });
+
+    it('should return all class attributes from append', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("
+                <strong class="selected secondClass">Hello</strong>
+                <div class="selected1 secondClass1">Hello</div>
+                " );`);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass', 'selected1', 'secondClass1'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
+    });
+
+    it('should find all classes from "append" by using multiple append but only once', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("
+                <strong class="selected secondClass">Hello</strong>
+                <div class="selected secondClass">Hello</div>
+                " );`);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
+
+    });
+
+    it('should find all ids from append(someHtmlWithId)', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("
+                <strong class="selected secondClass">Hello</strong>
+                <div id="myId" class="selected secondClass">Hello</div>
+                <div id="myId2">Hello</div>
+                " );`);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['selected', 'secondClass'], _id: ['myId', 'myId2'] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
     });
 
     xit('should ignore addClass and append comments ', (done) => {
 
+    });
+
+    it('should return all attributes from addClass and append', (done) => {
+        spyOn(s._util, 'readFile').andReturn(
+            new Promise((resolve, reject) => {
+                resolve(`$( "p" ).append("
+                <strong class="selected secondClass">Hello</strong>
+                <div class="selected secondClass">Hello</div>
+                " );
+                $( "p:last" ).addClass( "X Y" );
+                `);
+            })
+        );
+        s.findAttribute('some.js').then((selectors) => {
+            expect(selectors).toEqual({ _class: ['X', 'Y', 'selected', 'secondClass'], _id: [] });
+            done();
+        }, (err) => {
+            expect(err).not.toBeDefined();
+            done();
+        });
     });
 });
--- src/attributeFinder/jQuery.js
+++ src/attributeFinder/jQuery.js
@@ -2,6 +2,7 @@
 
 let Strategy = require('./strategy').AbstractAttributeFinder;
 const classRegex = /addClass.?\(.?[^ +function]([^)]+)\)/g;
+const appendRegex = /append.?\(.?[^ +function]([^)]+)\)/g;
 const parenthesesRegex = /\(([^)]+)\)/g;
 
 class jQueryAttributeFinder extends Strategy {
@@ -17,14 +18,15 @@ class jQueryAttributeFinder extends Strategy {
     classText = classText.replace(/\s\s+/g, ' '); // replace multiple empty chars with
     classList = classText.split(' ');
     for (let _clazz of classList) {
-      if(_clazz) {
-        this.attributes._class.push(_clazz);
+      if(_clazz && this.attributes._class.indexOf(_clazz) === -1) {
+            this.attributes._class.push(_clazz);
       }
     }
   }
   findAttribute(reactFile) {
     return new Promise((resolve, reject) => {
       this._util.readFile(reactFile).then((file) => {
+        let resultPromiseList = [];
         if (classRegex.test(file)) {
           let addClassStrList = (file.match(classRegex));
           for (let returnStr of addClassStrList) {
@@ -34,11 +36,38 @@ class jQueryAttributeFinder extends Strategy {
               this.setClassAttr(classText);
             }
           }
-          resolve(this.attributes);
+
+        }
+
+        if (appendRegex.test(file)) {
+          let htmlList = (file.match(appendRegex));
+          for (let html of htmlList) {
+            let classText = (html.match(parenthesesRegex))[0];
+
+            if (html) {
+               resultPromiseList.push(this._util.findHTMLAttributes(html));
+            }
+          }
+
+        }
+
+        if(resultPromiseList.length>0) {
+          Promise.all(resultPromiseList).then((resultList) => {
+            for (let attr of resultList) {
+              this.attributes._class = (this.attributes._class).concat(attr._class.filter((item) => {
+                return this.attributes._class.indexOf(item) < 0;
+              }));
+              this.attributes._id = (this.attributes._id).concat(attr._id.filter((item) => {
+                return this.attributes._id.indexOf(item) < 0;
+              }));
+            }
+            resolve(this.attributes);
+          }, (err) => {
+            reject(err);
+          });
         } else {
           resolve(this.attributes);
         }
-
       }, (err) => {
         reject(err);
       });
